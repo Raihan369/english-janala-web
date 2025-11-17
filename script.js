5a4e6ed792be6810814da5d847d42658d0548e8e
Raihan369 <b190305008@cse.jnu.ac.bd>
2025-11-17 22:52:48 +0600

    audio update

--- script.js
+++ script.js
@@ -5,6 +5,12 @@ const createSynonym = (arr) =>
     return (synonymContainer.join(" "));
 };
 
+function pronounceWord(word) {
+  const utterance = new SpeechSynthesisUtterance(word);
+  utterance.lang = "en-EN"; // English
+  window.speechSynthesis.speak(utterance);
+}
+
 const manageSpinner = (status) =>
 {
     if(status === true)
@@ -136,7 +142,7 @@ const displayLevelWord = (words) => {
 
             <div class="flex justify-between items-center">
                 <button onclick="loadWordDetail(${word.id})" class="btn bg-[#1A91FF10] hover:bg-[#1A91FF60]"><i class="fa-solid fa-circle-info"></i></button>
-                <button class="btn bg-[#1A91FF10] hover:bg-[#1A91FF60]"><i class="fa-solid fa-volume-high"></i></button>
+                <button onclick="pronounceWord('${word.word}')" class="btn bg-[#1A91FF10] hover:bg-[#1A91FF60]"><i class="fa-solid fa-volume-high"></i></button>
             </div>
 
         </div>
